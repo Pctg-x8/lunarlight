7e076fa193ef50cd733f221fc9e763082bdc18e2
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-03-03 00:56:08 +0900

    rm: unused font import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import styles from "@/styles/Home.module.css";
-import { Inter } from "@next/font/google";
 import { Metadata } from "next";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = { title: { default: "Lunarlight", template: "%s - Lunarlight" } } satisfies Metadata;
 
