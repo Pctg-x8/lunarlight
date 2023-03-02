da32b5039e0d45f31788a034e7f7e440311334e6
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-03-03 01:05:23 +0900

    fix: title template location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,12 @@ import Header from "@/components/Header";
 import "@/styles/globals.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Metadata } from "next";
 import React from "react";
 
 config.autoAddCss = false;
+
+export const metadata = { title: { default: "Lunarlight", template: "%s - Lunarlight" } } satisfies Metadata;
 
 export default function App({ children }: { readonly children: React.ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import styles from "@/styles/Home.module.css";
-import { Metadata } from "next";
-
-export const metadata = { title: { default: "Lunarlight", template: "%s - Lunarlight" } } satisfies Metadata;
 
 export default function Home() {
   return (
