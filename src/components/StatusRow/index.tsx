5ac9d9588a49061161c7fa18ccfa5e9fdecb275d
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-03-12 10:30:06 +0900

    add: click post to navigate

--- src/components/StatusRow/index.tsx
+++ src/components/StatusRow/index.tsx
@@ -1,12 +1,36 @@
 import { Status } from "@/models/api/mastodon/status";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 import AgoLabel from "../AgoLabel";
 import StatusActions from "../StatusActions";
 import styles from "./styles.module.scss";
 
 export default function StatusRow({ status }: { readonly status: Status }) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!contentRef.current) return;
+
+    const cancellation = new AbortController();
+    function recursiveProcessLink(e: ChildNode) {
+      if (e instanceof HTMLAnchorElement || e instanceof HTMLButtonElement) {
+        e.addEventListener("click", (e) => e.stopPropagation(), { signal: cancellation.signal });
+      }
+
+      for (const child of e.childNodes) recursiveProcessLink(child);
+    }
+    for (const child of contentRef.current.childNodes) recursiveProcessLink(child);
+    return () => cancellation.abort();
+  }, [contentRef]);
+
+  const nav = useRouter();
+
   return (
-    <article className={styles.statusRow}>
+    <article
+      ref={contentRef}
+      className={styles.statusRow}
+      onClick={() => nav.push(`/@${status.account.acct}/${status.id}`)}
+    >
       <Link className={`${styles.avatar} clickableImage`} href={`/@${status.account.acct}`}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={status.account.avatar} alt={status.account.acct} />
