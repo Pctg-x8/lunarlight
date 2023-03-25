908352c7c8575e6204a206c6a01f10b7989af71a
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-03-25 23:39:10 +0900

    fix: ignore prisma migration data

--- prisma/migrations/20230325082043_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ConnectedInstance" (
-    "domain" TEXT NOT NULL PRIMARY KEY,
-    "id" TEXT NOT NULL,
-    "client_id" TEXT NOT NULL,
-    "client_secret" TEXT NOT NULL,
-    "vapid_key" TEXT NOT NULL
-);
