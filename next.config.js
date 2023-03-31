0786a4bb8598265ef8c8c17c924f4e8424437f67
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-04-01 02:32:52 +0900

    add: linaria and login button design

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withLinaria = require("next-with-linaria");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -20,4 +22,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withLinaria(nextConfig);
--- src/components/LoginStatus/index.tsx
+++ src/components/LoginStatus/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { rpcClient } from "@/rpc/client";
+import { styled } from "@linaria/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import useSWR from "swr";
-import styles from "./styles.module.scss";
 
 export default function LoginStatus() {
   const { data: account, isLoading } = useSWR("authorizedAccount", () => rpcClient.authorizedAccount.query());
@@ -24,13 +24,67 @@ export default function LoginStatus() {
   if (isLoading) return <></>;
 
   return !account ? (
-    <button onClick={doLogin} disabled={loginPending} className={styles.button}>
+    <LoginButton onClick={doLogin} disabled={loginPending}>
       ログイン
-    </button>
+    </LoginButton>
   ) : (
-    <Link href={`/@${account.acct}`} title={`@${account.acct}`} className={`no-default ${styles.account}`}>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={account.avatar} alt={account.acct} />
-    </Link>
+    <AccountLink>
+      <Link href={`/@${account.acct}`} title={`@${account.acct}`} className="no-default">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={account.avatar} alt={account.acct} />
+      </Link>
+    </AccountLink>
   );
 }
+
+const LoginButton = styled.button`
+  display: block;
+  height: 32px;
+  width: 88px;
+  cursor: pointer;
+  border: none;
+
+  border-radius: 8px;
+  color: var(--theme-primary-button-text);
+  background: var(--theme-primary-button-bg);
+  box-shadow: inset 0 0px 0px #000;
+  transition: background 0.1s ease, box-shadow 0.1s ease;
+
+  &:hover {
+    background: var(--theme-primary-button-bg-hover);
+  }
+
+  &:not(:disabled):active {
+    background: var(--theme-primary-button-bg-active);
+    box-shadow: inset 0 0px 4px #000;
+  }
+
+  &:disabled {
+    background: var(--theme-primary-button-bg-disable);
+    color: var(--theme-primary-button-text-disable);
+    cursor: default;
+  }
+`;
+
+const AccountLink = styled.section`
+  & > a {
+    display: block;
+    width: 40px;
+    height: 40px;
+
+    border-radius: 4px;
+    box-shadow: 0 0 8px var(--theme-default-text);
+    cursor: pointer;
+    transition: box-shadow 0.2s ease-out;
+
+    &:hover {
+      box-shadow: 0 0 16px var(--theme-default-text);
+    }
+
+    & > img {
+      width: 100%;
+      height: 100%;
+      border-radius: 4px;
+    }
+  }
+`;
