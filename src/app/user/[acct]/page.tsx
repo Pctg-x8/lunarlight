27e84075b11765887436c4592e3d6a0d490c202d
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-04-01 11:44:42 +0900

    fix: refactor account page

--- src/app/user/[acct]/page.tsx
+++ src/app/user/[acct]/page.tsx
@@ -3,12 +3,13 @@ import UserHeader from "@/components/UserHeader";
 import { DefaultInstance, HTTPError, SearchParamsRequestBody } from "@/models/api";
 import { lookup } from "@/models/api/mastodon/account";
 import Webfinger from "@/models/webfinger";
+import { stripPrefix } from "@/utils";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 async function getData(acct: string) {
   try {
-    const account = await lookup.send(new SearchParamsRequestBody({ acct: decodeURIComponent(acct) }), DefaultInstance);
+    const account = await lookup.send(new SearchParamsRequestBody({ acct }), DefaultInstance);
     const fullAcct = await Webfinger.Address.decompose(account.acct).resolveDomainPart(DefaultInstance);
 
     return { account, fullAcct: fullAcct.toString() };
@@ -22,14 +23,14 @@ async function getData(acct: string) {
 }
 
 export async function generateMetadata({ params }: { readonly params: { readonly acct: string } }): Promise<Metadata> {
-  const { account, fullAcct } = await getData(params.acct.slice(3));
+  const { account, fullAcct } = await getData(stripPrefix(decodeURIComponent(params.acct), "@"));
 
   return { title: `${account.display_name}(@${fullAcct})` };
 }
 
 export default async function UserPage({ params }: { readonly params: { readonly acct: string } }) {
   // strip prefixing @(%40)
-  const { account, fullAcct } = await getData(params.acct.slice(3));
+  const { account, fullAcct } = await getData(stripPrefix(decodeURIComponent(params.acct), "@"));
 
   return (
     <>
