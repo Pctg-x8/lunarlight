1769096891f298c8ed2d3e0f0b63f5653624f569
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-04-01 10:45:13 +0900

    add: pagination for account timeline

--- src/components/AccountTimelnie/index.tsx
+++ src/components/AccountTimelnie/index.tsx
@@ -1,16 +1,43 @@
 "use client";
 
+import { Status } from "@/models/api/mastodon/status";
 import { rpcClient } from "@/rpc/client";
+import { useEffect, useMemo, useRef } from "react";
 import useSWRInfinite from "swr/infinite";
 import Timeline from "../Timeline";
 
 export default function AccountTimeline({ accountId }: { readonly accountId: string }) {
-  const { data, isLoading } = useSWRInfinite(
-    (_, prevPageData): { readonly max_id?: string; readonly limit?: number } => {
-      return !prevPageData ? { limit: 20 } : { limit: 20, max_id: prevPageData[prevPageData.length - 1].id };
+  const { data, isLoading, setSize } = useSWRInfinite(
+    (pageId, prevPageData): { readonly max_id?: string; readonly limit?: number } | null => {
+      console.log(pageId, prevPageData);
+      if (!prevPageData) return { limit: 20 };
+      if (prevPageData.length === 0) return null;
+      return { limit: 20, max_id: prevPageData[prevPageData.length - 1].id };
     },
     (req) => rpcClient.account.statuses.query({ accountId, ...req })
   );
+  const statuses: Status[] = useMemo(() => data?.flat() ?? [], [data]);
+  const sentinelRef = useRef<HTMLDivElement>(null);
 
-  return isLoading ? <p>Loading...</p> : <Timeline statuses={data?.flat() ?? []} />;
+  useEffect(() => {
+    if (!sentinelRef.current) return;
+
+    const io = new IntersectionObserver(
+      (_) => {
+        setSize((x) => x + 1);
+      },
+      { threshold: 1.0 }
+    );
+    io.observe(sentinelRef.current);
+    return () => io.disconnect();
+  }, [isLoading, setSize]);
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <>
+      <Timeline statuses={statuses} />
+      <div ref={sentinelRef} />
+    </>
+  );
 }
