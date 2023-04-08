f62ae577bb2bd854d596038d3fc728fd95202bac
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-04-08 20:38:42 +0900

    add: sidemenu entry

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,10 +1,43 @@
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { styled } from "@linaria/react";
+import Link from "next/link";
 
 export default function SideMenu(): JSX.Element {
-  return <Frame>SideMenu</Frame>;
+  return (
+    <Frame>
+      <ul>
+        <li>
+          <Link href="/" className="no-default">
+            <FontAwesomeIcon icon={faHouseChimney} />
+            Home
+          </Link>
+        </li>
+      </ul>
+    </Frame>
+  );
 }
 
 const Frame = styled.nav`
   height: fit-content;
   background: var(--theme-menu-background);
+
+  & > ul > li > a {
+    display: block;
+    padding: 16px;
+    text-decoration: none;
+
+    transition: background 0.1s ease;
+    background: rgb(255 255 255 / 0%);
+    color: var(--theme-subtext);
+
+    &:hover {
+      background: rgb(255 255 255 / 5%);
+      color: var(--theme-default-text);
+    }
+
+    & > svg {
+      margin-right: 16px;
+    }
+  }
 `;
