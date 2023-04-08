c2114a7c947ceaf43fa103f20a5443604763646c
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-04-08 12:57:12 +0900

    add: error handling for rpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,13 @@
 import { appRpcRouter, createContext } from "@/rpc/app";
 import * as trpcNext from "@trpc/server/adapters/next";
+import pino from "pino";
+
+const AppLogger = pino({ name: "trpc-error" });
 
 export default trpcNext.createNextApiHandler({
   router: appRpcRouter,
   createContext,
+  onError: ({ error, path, input }) => {
+    AppLogger.error({ error, path, input });
+  }
 });
