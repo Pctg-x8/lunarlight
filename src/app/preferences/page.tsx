981bf4795d0ff304f31a744fc7f7987365288df5
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-04-09 11:47:08 +0900

    fix: preferences page width

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -1,12 +1,18 @@
 import TimelineModePreferences from "@/components/Preferences/TimelineMode";
+import { styled } from "@linaria/react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = { title: "Preferences" };
 
 export default function PreferencesPage(): JSX.Element {
   return (
-    <>
+    <Frame>
       <TimelineModePreferences />
-    </>
+    </Frame>
   );
 }
+
+const Frame = styled.main`
+  max-width: var(--single-max-width);
+  width: 100%;
+`;
