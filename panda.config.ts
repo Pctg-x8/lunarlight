2583994cf7c92f2c9bf77fa4b2e53d03046b8001
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-07-09 20:03:58 +0900

    fix: rewrite header style

--- panda.config.ts
+++ panda.config.ts
@@ -1,22 +1,45 @@
-import { defineConfig } from "@pandacss/dev"
+import { defineConfig, defineGlobalStyles } from "@pandacss/dev";
 
 export default defineConfig({
-    // Whether to use css reset
-    preflight: true,
-    
-    // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  // Whether to use css reset
+  preflight: true,
 
-    // Files to exclude
-    exclude: [],
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
 
-    // Useful for theme customization
-    theme: {
-      extend: {}
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {},
+    semanticTokens: {
+      colors: {
+        app: {
+          background: { value: "rgb(24, 28, 42)" },
+          text: { value: "#ffe" },
+        },
+        appheader: {
+          bottom: { value: "#046" },
+          // TODO: これなんとかapp.backgroundからの派生の形で書けないか？
+          background: { value: "rgba(24 28 42 / 60%)" },
+        },
+        label: {
+          normal: {
+            background: { value: "#9df" },
+            text: { value: "{colors.app.background}" },
+          },
+        },
+      },
     },
+  },
+  globalCss: defineGlobalStyles({
+    body: {
+      color: "app.text",
+      background: "app.background",
+    },
+  }),
 
-    // The output directory for your css system
-    outdir: "styled-system",
-    
-    
-})
+  // The output directory for your css system
+  outdir: "styled-system",
+});
