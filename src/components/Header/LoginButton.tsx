3b0a9d088cabc2dcc030bab49f8bf7fc280bb60c
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-07-10 21:53:19 +0900

    fix: split base design ui

--- src/components/Header/LoginButton.tsx
+++ src/components/Header/LoginButton.tsx
@@ -1,39 +1,23 @@
 "use client";
 
 import { doLogin } from "@/actions/login";
-import { css } from "@styled-system/css";
 import { useTransition } from "react";
+import Button from "../common/Button";
 
 export default function LoginButton() {
   const [pending, startTransition] = useTransition();
 
   return (
-    <button className={PrimaryButton} type="button" onClick={() => startTransition(() => doLogin())} disabled={pending}>
+    <Button
+      variant="primary"
+      display="block"
+      height="32px"
+      width="88px"
+      type="button"
+      onClick={() => startTransition(() => doLogin())}
+      disabled={pending}
+    >
       ログイン
-    </button>
+    </Button>
   );
 }
-
-const PrimaryButton = css({
-  display: "block",
-  height: "32px",
-  width: "88px",
-  cursor: "pointer",
-  borderRadius: "8px",
-  color: "button.primary.text.default",
-  background: "button.primary.background.default",
-  boxShadow: "inset 0 0 0 #000",
-  transition: "background 0.1s ease, box-shadow 0.1s ease",
-  _hover: {
-    background: "button.primary.background.hover",
-  },
-  _active: {
-    background: "button.primary.background.active",
-    boxShadow: "inset 0 0 4px #000",
-  },
-  _disabled: {
-    background: "button.primary.background.disabled !important",
-    color: "button.primary.text.disabled",
-    cursor: "default",
-  },
-});
