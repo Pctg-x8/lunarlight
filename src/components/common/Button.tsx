3dc168395233df8e9012e6e33c4b406df2bdc29f
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-07-10 23:30:29 +0900

    fix: revert common button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,31 +1,33 @@
-import { styled } from "@linaria/react";
+import { styled } from "@styled-system/jsx";
 
-export const Button = styled.button<{ readonly primary: boolean }>`
-  display: block;
-  padding: 0.4rem 0.8rem;
-  cursor: pointer;
-  border: ${props => (props.primary || props.disabled ? "none" : "var(--theme-primary-button-text) 1px solid")};
-
-  border-radius: 8px;
-  color: var(--theme-primary-button-text);
-  background: ${props => (props.primary ? "var(--theme-primary-button-bg)" : "var(--theme-button-bg)")};
-  box-shadow: inset 0 0px 0px #000;
-  transition: background 0.1s ease, box-shadow 0.1s ease, transform 0.1s ease;
-  transform: scale(1);
-
-  &:hover {
-    background: ${props => (props.primary ? "var(--theme-primary-button-bg-hover)" : "var(--theme-button-bg-hover)")};
-  }
-
-  &:not(:disabled):active {
-    background: ${props => (props.primary ? "var(--theme-primary-button-bg-active)" : "var(--theme-button-bg-active)")};
-    box-shadow: inset 0 0px 4px #000;
-    transform: scale(0.97);
-  }
-
-  &:disabled {
-    background: var(--theme-button-bg-disable);
-    color: var(--theme-button-text-disable);
-    cursor: default;
-  }
-`;
+const Button = styled("button", {
+  base: {
+    cursor: "pointer",
+    borderRadius: "8px",
+    boxShadow: "inset 0 0 0 #000",
+    transition: "background 0.1s ease, box-shadow 0.1s ease",
+    _active: {
+      boxShadow: "inset 0 0 4px #000",
+    },
+    _disabled: {
+      cursor: "default",
+      bg: "button.primary.background.disabled !important",
+      color: "button.primary.text.disabled !important",
+    },
+  },
+  variants: {
+    variant: {
+      primary: {
+        color: "button.primary.text.default",
+        bg: "button.primary.background.default",
+        _hover: {
+          bg: "button.primary.background.hover",
+        },
+        _active: {
+          bg: "button.primary.background.active",
+        },
+      },
+    },
+  },
+});
+export default Button;
