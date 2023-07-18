4d299089809ab2b0c777b05ae72bafaddea23238
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-07-18 10:58:48 +0900

    fix: use dedicated expert timeline view

--- src/components/HomeStreamingTimeline.tsx
+++ src/components/HomeStreamingTimeline.tsx
@@ -8,7 +8,7 @@ import Immutable from "immutable";
 import { useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
 import useSWRInfinite from "swr/infinite";
 import { ClientPreferencesContext } from "./ClientPreferencesProvider";
-import Timeline from "./Timeline";
+import ExpertTimelineView from "./Timeline/Expert";
 import NormalTimelineView from "./Timeline/Normal";
 
 export default function HomeStreamingTimeline() {
@@ -59,7 +59,7 @@ export default function HomeStreamingTimeline() {
       {mode === "normal" ? (
         <NormalTimelineView statuses={statuses} deletedIds={deletedIds} />
       ) : (
-        <Timeline statuses={statuses} deletedIds={deletedIds} mode={mode} />
+        <ExpertTimelineView statuses={statuses} deletedIds={deletedIds} />
       )}
       <div ref={sentinelRef} />
     </>
