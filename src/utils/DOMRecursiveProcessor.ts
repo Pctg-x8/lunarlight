8450fbcf8b252d30782cc74a5aed3d99e791ad1a
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-07-22 18:34:12 +0900

    fix: simplification recursive processor

--- src/utils/DOMRecursiveProcessor.ts
+++ src/utils/DOMRecursiveProcessor.ts
@@ -1,8 +1,6 @@
 export function recursiveProcessDOMNodes(root: Node, action: (element: Node) => void) {
-  function rec(x: ChildNode) {
-    action(x);
-    for (const child of x.childNodes) rec(child);
+  for (const child of root.childNodes) {
+    action(child);
+    recursiveProcessDOMNodes(child, action);
   }
-
-  for (const child of root.childNodes) rec(child);
 }
